ce455a0dabf7f9daad2053c3b88bbea0694abe2a
Steven M. Robbins <stever@bic.mni.mcgill.ca>
2001-07-25 14:42:30 +0000

    Hack to make SGI CC happy.

--- lib/bicInventor.h
+++ lib/bicInventor.h
@@ -2,6 +2,10 @@
 #define BICINVENTOR_H
 
 class SoNode;
+
+#if 0
+/** SGI's CC has a problem with redeclaring structs,
+    though oddly it is okay with redeclaring classes.... **/
 struct lines_struct;
 struct marker_struct;
 struct polygons_struct;
@@ -11,6 +15,7 @@ SoNode* bic_lines_to_iv( const lines_struct& l );
 SoNode* bic_marker_to_iv( const marker_struct& m );
 SoNode* bic_polygons_to_iv( const polygons_struct& p );
 SoNode* bic_quadmesh_to_iv( const quadmesh_struct& q );
+#endif
 
 //! Convert entire BIC .obj file to scene graph.
 SoNode* bic_graphics_file_to_iv( char* filename );
